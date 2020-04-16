8080990d72abbcc2c6b15f8b112dc15f645416e6
Chris Kraft <kriggity@gmail.com>
2020-04-15 22:05:34 -0400

    jeopardy

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Footer.css';
+import Jeopardy from '../james/jeopardyfetch';
 
 export default function Footer() {
     return (
         <footer className="App-footer">
             <p>&copy; TwoForTP 2020</p>
+            <Jeopardy />
         </footer>
     );
 }
