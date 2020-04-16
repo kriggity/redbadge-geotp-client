0042c0b14195e418d1b3e54c8cbea600d8263d76
Chris Kraft <kriggity@gmail.com>
2020-04-15 23:09:41 -0400

    styling; added Tacos back in

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Typography, Box } from '@material-ui/core';
+import TacoFancy from '../TacoFancy/TacoFancy';
 import './Footer.css';
 
 export default function Footer() {
     return (
         <footer className="App-footer">
-            <p>&copy; TwoForTP 2020</p>
+            <Typography component="div">
+                <Box textAlign="center">
+                    <p>&copy; TwoForTP 2020</p>
+                </Box>
+            </Typography>
+            <TacoFancy />
         </footer>
     );
 }
